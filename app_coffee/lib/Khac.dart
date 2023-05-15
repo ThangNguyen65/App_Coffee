2cdace2d6e1651834e6164ed5fe5fd0e2a65ad3d
duongphatloc <locdp21.cdsg@saigontech.edu.vn>
2023-05-16 04:39:14 +0700

    loc

--- app_coffee/lib/Khac.dart
+++ app_coffee/lib/Khac.dart
@@ -62,21 +62,85 @@ class _KhacState extends State<Khac> {
                             fontSize: 20,
                           ),
                         ),
-                        // Icon(Icons.arrow_forward_ios_rounded),
                       ],
                     ),
                     SizedBox(
                       height: 20,
                     ),
                     Container(
+                      padding: EdgeInsets.all(16),
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(16),
                       ),
-                      child: ListTile(leading: Icon(Icons.front)),
-                    )
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(children: [
+                            Icon(Icons.account_box_rounded),
+                            SizedBox(
+                              width: 12,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Hồ sơ",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ]),
+                          Icon(Icons.arrow_forward_ios_rounded),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 9,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(children: [
+                            Icon(Icons.settings),
+                            SizedBox(
+                              width: 12,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Cài đặt",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ]),
+                          Icon(Icons.arrow_forward_ios_rounded),
+                        ],
+                        
+                      ),
+                      
+                        
+                    ),
+                    
                   ],
+                  
+                  
                 ),
+                
               ),
             ),
           ],
