7b26e8d70d7ccfe3259aa211fffd8c45b7376297
Huỳnh Ngân <nganhn21.cdsg@saigontech.edu.vn>
2023-05-30 18:41:59 +0700

    Update Describe, Describe_Store, Display_Store, Search

---
+++ app_coffee/lib/Describe.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+class Describe {
+  String name;
+  String address;
+  String time;
+  String imageUrl;
+  String imageUrl1;
+  String imageUrl2;
+  String imageUrl3;
+  String imageUrl4;
+
+  String detailed_address;
+  String favorite;
+  String phone;
+
+  Describe(
+      {required this.imageUrl,
+      required this.imageUrl1,
+      required this.imageUrl2,
+      required this.imageUrl3,
+      required this.imageUrl4,
+      required this.name,
+      required this.address,
+      required this.time,
+      required this.detailed_address,
+      required this.favorite,
+      required this.phone});
+}
+
+List<Describe> describeList = [
+  Describe(
+      imageUrl:
+          'https://i.pinimg.com/564x/70/fd/63/70fd633ca9a25cb64b612eeb17fd3cc6.jpg',
+      imageUrl1:
+          'https://i.pinimg.com/564x/70/fd/63/70fd633ca9a25cb64b612eeb17fd3cc6.jpg',
+      imageUrl2:
+          'https://i.pinimg.com/564x/fc/34/df/fc34df9d70e2408699b13c95126b0deb.jpg',
+      imageUrl3:
+          'https://i.pinimg.com/564x/a3/2e/ae/a32eae835305c6c01b301c64fc14ded3.jpg',
+      imageUrl4:
+          'https://i.pinimg.com/564x/89/3a/2f/893a2f5c800243084459f658afeb0547.jpg',
+      name: 'Cái tiệm Coffee',
+      address: '296 Quang Trung',
+      time: 'Giờ mở cửa: 7:00 - 22:00',
+      detailed_address: '293 Quang Trung, Quận Gò Vấp, Hồ Chí Minh, Việt Nam',
+      favorite: 'Thêm vào danh sách yêu thích',
+      phone: 'Liên hệ '),
+  Describe(
+      imageUrl:
+          'https://i.pinimg.com/736x/39/79/40/397940651ea275b14007db82acf00349.jpg',
+      imageUrl1:
+          'https://i.pinimg.com/564x/79/ff/0a/79ff0a837ece9958ccb774e1b2418796.jpg',
+      imageUrl2:
+          'https://i.pinimg.com/564x/11/25/ce/1125ceb6de262799e4634b14d240aa09.jpg',
+      imageUrl3:
+          'https://i.pinimg.com/564x/f0/c7/7c/f0c77c83aa1e28a19ab3dd27b8216b3e.jpg',
+      imageUrl4:
+          'https://i.pinimg.com/564x/38/44/63/384463d2ed6c37fae0a42c1e3377b4ff.jpg',
+      name: 'Cái tiệm Coffee',
+      address: '86 Cao Thắng',
+      time: 'Giờ mở cửa: 7:00 - 22:00',
+      detailed_address: '86 Cao Thắng, Quận 3, Hồ Chí Minh, Việt Nam',
+      favorite: 'Thêm vào danh sách yêu thích',
+      phone: 'Liên hệ '),
+  Describe(
+      imageUrl:
+          'https://i.pinimg.com/564x/96/9e/c2/969ec2b5d0d09fd5bfb9e67ba9ec5174.jpg',
+      imageUrl1:
+          'https://i.pinimg.com/564x/86/2c/63/862c6327cdc608c1a062e2ee12f0fd9b.jpg',
+      imageUrl2:
+          'https://i.pinimg.com/564x/f2/cd/1b/f2cd1b2e6844027116059fe12a3cce3f.jpg',
+      imageUrl3:
+          'https://i.pinimg.com/564x/cf/34/fe/cf34fe17c665a7bc732bb78fc296405d.jpg',
+      imageUrl4:
+          'https://i.pinimg.com/564x/10/f1/ea/10f1ea94db80e814b5aea64764b9719e.jpg',
+      name: 'Cái tiệm Coffee',
+      address: '798 Sư Vạn Hạnh',
+      time: 'Giờ mở cửa: 7:00 - 22:00',
+      detailed_address: '798 Sư Vạn Hạnh, Quận 10, Hồ Chí Minh, Việt Nam',
+      favorite: 'Thêm vào danh sách yêu thích',
+      phone: 'Liên hệ '),
+  Describe(
+      imageUrl:
+          'https://i.pinimg.com/564x/a3/b1/3b/a3b13b6d79651e8a89c4e1c9e27e0bbd.jpg',
+      imageUrl1:
+          'https://i.pinimg.com/564x/a8/b6/6c/a8b66c3d005c0246cecd986b7c89a780.jpg',
+      imageUrl2:
+          'https://i.pinimg.com/564x/6f/36/94/6f369409c0fa2cc267c58dddcac9b3b2.jpg',
+      imageUrl3:
+          'https://i.pinimg.com/564x/e7/a9/f8/e7a9f822aee54315634cac6d6a4de6f7.jpg',
+      imageUrl4:
+          'https://i.pinimg.com/564x/68/a4/a4/68a4a435fcbb23575fc5178aa0515b76.jpg',
+      name: 'Cái tiệm Coffee',
+      address: '95 Nguyễn Ảnh Thủ',
+      time: 'Giờ mở cửa: 7:00 - 22:00',
+      detailed_address:
+          '95 Nguyễn Ảnh Thủ, Huyện Hóc Môn, Hồ Chí Minh, Việt Nam',
+      favorite: 'Thêm vào danh sách yêu thích',
+      phone: 'Liên hệ '),
+  Describe(
+      imageUrl:
+          'https://vietworld.world/Userfiles/Upload/images/281353874_1070179267039723_2436192077224018683_n.jpg',
+      imageUrl1:
+          'https://i.pinimg.com/564x/0f/cd/aa/0fcdaaa06079fc76a666be0e1bd64620.jpg',
+      imageUrl2:
+          'https://i.pinimg.com/564x/da/a0/b4/daa0b4b8db5f396bdba18a34fa71fb68.jpg',
+      imageUrl3:
+          'https://i.pinimg.com/564x/92/a9/c6/92a9c6484ecf5e91574289b18322270e.jpg',
+      imageUrl4:
+          'https://i.pinimg.com/564x/1b/76/1a/1b761a5c9addce405b358ab4113c4636.jpg',
+      name: 'Cái tiệm Coffee',
+      address: '55 Thống Nhất',
+      time: 'Giờ mở cửa: 7:00 - 22:00',
+      detailed_address: '55 Thống Nhất, Quận Gò Vấp, Hồ Chí Minh, Việt Nam',
+      favorite: 'Thêm vào danh sách yêu thích',
+      phone: 'Liên hệ '),
+  Describe(
+      imageUrl: 'https://noithatsaigonaz.vn/uploads/source/ca-phe/cf44.jpg',
+      imageUrl1:
+          'https://i.pinimg.com/564x/cf/68/e4/cf68e465680297a2333d11b0cd5732f8.jpg',
+      imageUrl2:
+          'https://i.pinimg.com/564x/eb/f6/c2/ebf6c2b8060c2664ba81381b1790f9f1.jpg',
+      imageUrl3:
+          'https://i.pinimg.com/564x/fb/54/7f/fb547f11208fbfb02bf06b284361a747.jpg',
+      imageUrl4:
+          'https://i.pinimg.com/564x/be/50/1c/be501cf8978078adb8493a2d78ff9c9e.jpg',
+      name: 'Cái tiệm Coffee',
+      address: '27 Lê Lợi',
+      time: 'Giờ mở cửa: 7:00 - 22:00',
+      detailed_address: '27 Lê Lợi, Thành phố Thanh Hóa, Thanh Hóa, Việt Nam',
+      favorite: 'Thêm vào danh sách yêu thích',
+      phone: 'Liên hệ '),
+  Describe(
+      imageUrl:
+          'https://i.pinimg.com/564x/1c/d0/78/1cd0785884201a3b953b6b94ad4cb58e.jpg',
+      imageUrl1:
+          'https://i.pinimg.com/564x/e1/ea/24/e1ea24157b2b8bb6e0242913b97dfa26.jpg',
+      imageUrl2:
+          'https://i.pinimg.com/564x/c2/50/64/c25064c2c57919c4efe3c69a47b78852.jpg',
+      imageUrl3:
+          'https://i.pinimg.com/564x/92/73/8f/92738f8bb36f93d404b458c1c97fc562.jpg',
+      imageUrl4:
+          'https://i.pinimg.com/564x/39/78/95/397895c57b7268f82034d2837218ac56.jpg',
+      name: 'Cái tiệm Coffee',
+      address: 'Song Hành',
+      time: 'Giờ mở cửa: 7:00 - 22:00',
+      detailed_address: 'Song Hành, Quận 2, Hồ Chí Minh, Việt Nam',
+      favorite: 'Thêm vào danh sách yêu thích',
+      phone: 'Liên hệ '),
+];
